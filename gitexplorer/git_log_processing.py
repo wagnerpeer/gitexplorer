1cc7b6cfb6cdb79b7691323ad5cda9e756afd51e
Peer Wagner <wagnerpeer@gmail.com>
2017-09-07 06:34:28 +0200

    Added logging, removed escaping of file path and fixed other issues

--- gitexplorer/git_log_processing.py
+++ gitexplorer/git_log_processing.py
@@ -4,12 +4,11 @@ Created on 19.08.2017
 @author: Peer
 '''
 import datetime
+import logging
 import os
 import pathlib
 import re
 import subprocess
-
-from .basics import GitExplorerBase as ge_base
 
 PATH_RENAME = re.compile('^(?P<prefix>[^{]*){?(?P<old>.*) => (?P<new>[^}]*)}?(?P<suffix>.*)$')
 PERMISSION_CHANGE = re.compile('(?P<old_permission>\d*) => (?P<new_permission>\d*)')
@@ -17,8 +16,13 @@ PERMISSION_CHANGE = re.compile('(?P<old_permission>\d*) => (?P<new_permission>\d
 
 class GitLogAnalyzer(object):
 
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)8s] [%(name)s] :: %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S',
+                        level=logging.INFO)
+    logger = logging.getLogger('gitexplorer')
+
     @classmethod
-    def _process_details(cls, changes):
+    def _process_details(cls, commit_hash, changes):
 
         details_dict = {'create': [],
                         'delete': [],
@@ -32,7 +36,7 @@ class GitLogAnalyzer(object):
                 if(line.startswith(('create', 'delete'))):
                     action, _, permission, file_path = line.split(' ', maxsplit=3)
                     create_delete = {'permission': int(permission),
-                                     'file_path': ge_base._mongodb_escape(file_path),
+                                     'file_path': file_path,
                                      'extension': pathlib.PurePath(file_path).suffix}
                     details_dict[action].append(create_delete)
                 elif(line.startswith('rename')):
@@ -40,12 +44,17 @@ class GitLogAnalyzer(object):
                     paths_combined, match = paths_and_match.rsplit(' ', maxsplit=1)
 
                     paths_match = PATH_RENAME.search(paths_combined)
+
+                    if(paths_match is None):
+                        cls.logger.warning('Unparsable rename in commit {} detected: "{}"'.format(commit_hash, line))
+                        continue
+
                     old_file_path = paths_match.group('prefix') + paths_match.group('old') + paths_match.group('suffix')
                     new_file_path = paths_match.group('prefix') + paths_match.group('new') + paths_match.group('suffix')
 
-                    rename = {'new_path': ge_base._mongodb_escape(new_file_path),
+                    rename = {'new_path': new_file_path,
                               'new_extension': pathlib.PurePath(new_file_path).suffix,
-                              'old_path': ge_base._mongodb_escape(old_file_path),
+                              'old_path': old_file_path,
                               'old_extension': pathlib.PurePath(old_file_path).suffix,
                               'match': int(match.strip('(%)'))}
                     details_dict['rename'].append(rename)
@@ -54,26 +63,36 @@ class GitLogAnalyzer(object):
                     permission_change, file_path = permission_change_and_filename.rsplit(' ', maxsplit=1)
 
                     permission_change_match = PERMISSION_CHANGE.search(permission_change)
+
+                    if(permission_change is None):
+                        cls.logger.warning('Unparsable permission change in commit {} detected: "{}"'.format(commit_hash, line))
+                        continue
+
                     old_permission = permission_change_match.group('old_permission')
                     new_permission = permission_change_match.group('new_permission')
 
                     change = {'old_permission': int(old_permission),
                               'new_permission': int(new_permission)}
 
-                    details_dict['change'][ge_base._mongodb_escape(file_path)] = change
+                    details_dict['change'][file_path] = change
                 elif(line[0] in '1234567890-'):
                     additions, deletions, file_path = line.split('\t', maxsplit=2)
 
                     if('=>' in file_path):
                         paths_match = PATH_RENAME.search(file_path)
+
+                        if(paths_match is None):
+                            cls.logger.warning('Unparsable path_rename in commit {} detected: "{}"'.format(commit_hash, line))
+                            continue
+
                         file_path = paths_match.group('prefix') + paths_match.group('new') + paths_match.group('suffix')
 
                     if(additions == '-'):
-                        modifications = {'file_path': ge_base._mongodb_escape(file_path),
+                        modifications = {'file_path': file_path,
                                          'additions': None,
                                          'deletions': None}
                     else:
-                        modifications = {'file_path': ge_base._mongodb_escape(file_path),
+                        modifications = {'file_path': file_path,
                                          'additions': int(additions),
                                          'deletions': int(deletions)}
                     details_dict['modifications'].append(modifications)
@@ -87,7 +106,12 @@ class GitLogAnalyzer(object):
         lines = commit.split('\n')
 
         if(lines):
-            commit_hash, author_name, author_mail, author_time = lines[0].split('\t')
+            commit_hash, author_name, author_mail, author_time, additional_information = lines[0].split('\t')
+
+            if(additional_information):
+                cls.logger.warning('Unexpected additional information: "{}"'.format(additional_information))
+            else:
+                cls.logger.info('Unexpected additional information: "{}"'.format(additional_information))
 
             commit_dict['commit_hash'] = commit_hash
             commit_dict['author'] = author_name
@@ -95,7 +119,7 @@ class GitLogAnalyzer(object):
             commit_dict['date'] = datetime.datetime.utcfromtimestamp(float(author_time))
 
             if(lines[1:]):
-                commit_dict['details'] = cls._process_details(lines[1:])
+                commit_dict['details'] = cls._process_details(commit_hash, lines[1:])
 
         return commit_dict
 
@@ -128,7 +152,7 @@ class GitLogReader(object):
 
     def get_log_information(self):
         '''Runs a "git log" command in the given directory and returns the parsed information.
-    '''
+        '''
 
         os.chdir(self.directory)
 
@@ -144,7 +168,7 @@ class GitLogReader(object):
                                           ' --summary'
                                           ' --find-renames'
                                           ' --reverse'
-                                          ' --pretty=format:"<gitexplorer>%H\t%aN\t%aE\t%at"'
+                                          ' --pretty=format:"<gitexplorer>%H\t%aN\t%aE\t%at\t"'
                                           + self.after
                                           + self.before,
                                           stderr=subprocess.STDOUT).decode("utf-8")
